ca967e2ce5fbeb780f67291bca4c492459b266eb
brunhs <xoqxoq@xoqxoq-500R5M-500R5W-501R5M>
2022-06-08 06:09:50 -0300

    Changing recommend course class

--- course-recommender-system-app/model/model.py
+++ course-recommender-system-app/model/model.py
@@ -1,38 +1,9 @@
 import pandas as pd
 from model.utils.utils import extractFeatures
 
-# class recommendCourse():
-
-#     def __init__(self):
-#         pass
-
-#     def recommendCourse(self, dataframe, title, cosine_sim_mat, numrec):
-
-#         course_index = pd.Series(
-#             dataframe.index, index=dataframe['course_title']).drop_duplicates()
-
-#         index = course_index[title]
-
-#         scores = list(enumerate(cosine_sim_mat[index]))
-
-#         sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
-
-#         selected_course_index = [i[0] for i in sorted_scores[0:]]
-
-#         selected_course_score = [i[1] for i in sorted_scores[0:]]
-
-#         rec_df = dataframe.iloc[selected_course_index].join(pd.DataFrame(selected_course_score,
-#  index=selected_course_index, columns=['Similarity_Score']))
-
-#         final_recommended_courses = rec_df[[
-#             'course_title', 'Similarity_Score', 'url', 'price', 'num_subscribers']]
-
-#         return final_recommended_courses.head(numrec)
-
 class recommendCourse():
 
     def __init__(self):
-        # self.course_dict = None
         pass
     
     def recommendCourse(self, dataframe, title, mat, numrec):
@@ -58,6 +29,6 @@ class recommendCourse():
 
         course_url, course_title, course_price = extractFeatures(final_recommended_courses)
 
-        self.course_dict = dict(zip(course_title, course_url))
+        course_dict = dict(zip(course_title, course_url))
 
-        return self.course_dict
+        return course_dict
