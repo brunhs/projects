b1c21fc3c597dac1ec33199159ea5ded6a043057
brunhs <xoqxoq@xoqxoq-500R5M-500R5W-501R5M>
2022-06-21 16:04:53 -0300

    last mods

---
+++ book-recommender-system-app/model/utils/utils.py
@@ -0,0 +1,30 @@
+def landing_generator(dataframe, amount, kind:str):
+    """
+    Loading screen generator
+
+    Args:
+        dataframe (dataframe): Dataframe.
+        amount (integer): Amount needed for recommendation.
+        kind (str): Kind of the recommendation.
+
+    Returns:
+        dictionary: Dictionary containing courses informations and links.
+    """
+
+    transformed_dataset = dataframe.groupby(
+    ['ISBN', 'Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher',
+       'Image-URL-S', 'Image-URL-M', 'Image-URL-L']
+       )\
+           .agg({'Book-Rating':['mean', 'count']})
+
+
+    if kind == 'top_rated':
+        formated_dataframe = transformed_dataset.sort_values(by=[('Book-Rating', 'mean'),('Book-Rating', 'count'), 'Year-Of-Publication'], ascending=False).head(amount)\
+            .droplevel(level=0, axis=1).reset_index().loc[:,['Book-Title', 'Image-URL-L']]
+        final_dict = dict(zip(formated_dataframe['Book-Title'], formated_dataframe['Image-URL-L']))
+ 
+    elif kind == 'reviewed':
+        formated_dataframe = transformed_dataset.sort_values(by=[('Book-Rating', 'count'),('Book-Rating', 'mean'), 'Year-Of-Publication'], ascending=False).head(amount)\
+            .droplevel(level=0, axis=1).reset_index().loc[:,['Book-Title', 'Image-URL-L']]
+        final_dict = dict(zip(formated_dataframe['Book-Title'], formated_dataframe['Image-URL-L']))
+    return final_dict
