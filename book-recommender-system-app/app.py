564b6e96bb6d411c7774cbfa9484b1eae6607d3f
brunhs <xoqxoq@xoqxoq-500R5M-500R5W-501R5M>
2022-06-29 10:07:54 -0300

    Adding new stuff and reseting databases

--- book-recommender-system-app/app.py
+++ book-recommender-system-app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, request
 from flask_sqlalchemy import SQLAlchemy
-from model.utils.utils import landing_generator
+from model.utils.utils import landing_generator, custom_id
+import uuid
 import pandas as pd
 
 app = Flask(__name__)
@@ -15,21 +16,32 @@ db = SQLAlchemy(app)
 # user schema
 class User(db.Model):
     __tablename__= 'user'
-    id=db.Column(db.Integer, primary_key=True)
-    fname=db.Column(db.String(50))
-    lname=db.Column(db.String(50))
+    user_id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    location=db.Column(db.String(50))
+    age=db.Column(db.Float)
+    uuid=db.Column(db.String(50), primary_key=True)
+    firstname=db.Column(db.String(50))
+    lastname=db.Column(db.String(50))
+
+    def __init__(self, uuid, firstname, lastname, user_id):
+        self.uuid = uuid
+        self.firstname = firstname
+        self.lastname = lastname
+        self.user_id = user_id
+
 
 # click schema
-class Clicks(db.Model):
-    __tablename__= 'clicks'
-    col_id=db.Column(db.Integer, primary_key=True)
-    user_id=db.Column(db.Integer)
+class Ratings(db.Model):
+    __tablename__= 'rating'
+    col_id=db.Column(db.String(50), primary_key=True)
+    uuid=db.Column(db.String(50))
+    fname=db.Column(db.String(50))
+    lname=db.Column(db.String(50))
     book_id=db.Column(db.Integer)
     isbn=db.Column(db.Integer)
     rate=db.Column(db.Integer)
-    firstname=db.Column(db.String(50))
 
-    def __init__(self, col_id, user_id, book_id, isbn, rate, firstname):
+    def __init__(self, col_id, uuid, book_id, isbn, rate, firstname):
         self.col_id = col_id
         self.user_id = user_id
         self.book_id = book_id
@@ -39,11 +51,16 @@ class Clicks(db.Model):
 
 
 # Search schema
-class Search(db.Model):
+class Searchs(db.Model):
     __tablename__= 'search'
-    user_id=db.Column(db.Integer, primary_key=True)
-    firstname=db.Column(db.String(50))
+    user_id=db.Column(db.String(50), primary_key=True)
     search=db.Column(db.String(50))
+    time=db.Column(db.DateTime)
+    def __init__(self, uuid, fname, lname):
+        self.uuid = uuid
+        self.fname = fname
+        self.lname = lname
+
 
 
 # route
@@ -54,13 +71,16 @@ def index():
 # user registration
 @app.route('/register',methods=['GET','POST'])
 def register():
+
     if request.method == 'POST':
+
+        user_id = custom_id()
         firstname = request.form['firstname']
         lastname = request.form['lastname']
         if firstname == '' or lastname == '':
             return render_template('register.html', message='Please include your first name AND your last name.')
 
-        single_user = User(fname=firstname,lname=lastname)
+        single_user = User(uuid=uuid.uuid4().hex,firstname=firstname,lastname=lastname, user_id=user_id)
         db.session.add(single_user)
         db.session.commit()
         session['firstname'] = firstname
@@ -73,6 +93,14 @@ def main():
         firstname = session.get('firstname')
         lastname = session.get('lastname')
 
+        bookmap = landing_generator(book_user_rating, amount=6, kind='top_rated')
+
+        return render_template('main.html', firstname=firstname, lastname=lastname, bookmap=bookmap)
+
+    if request.method == 'POST':
+        star1 = request.form('star')
+
+        print(star1)
         bookmap = landing_generator(book_user_rating, amount=6, kind='top_rated')
 
         return render_template('main.html', firstname=firstname, lastname=lastname, bookmap=bookmap)
