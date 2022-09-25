b900aaed2c3a77b2b08a1ac48a297366c60fe72a
brunhs <xoqxoq@xoqxoq-500R5M-500R5W-501R5M>
2022-09-25 18:46:52 -0300

    Adding functions descriptions

--- text-extraction-app/app/utils/image_treatment.py
+++ text-extraction-app/app/utils/image_treatment.py
@@ -14,24 +14,51 @@ class ImageExtract():
         self.image = image
 
     def img_to_array(self, image):
+        """
+        Convert image to and array of numbers.
+
+        Returns:
+            numpy.ndarray: Array containing converted image.
+        """        
         # Converting image to array
         image_arr = np.array(image.convert('RGB'))
         
         return image_arr
 
     def img_to_gray(self, image):
+        """
+        Convert image array to gray scale image.
+
+        Returns:
+            numpy.ndarray: Array containing converted image.
+        """        
         # Converting image to grayscale
         gray_img_arr = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         
         return gray_img_arr
 
     def img_to_rgb(self, image):
+        """
+        Convert image to rgb.
+
+        Args:
+            image (numpy.ndarray): numpy array of converted image.
+
+        Returns:
+            numpy.ndarray: array of image.
+        """        
         #Converting image back to rbg
         rgb_image = Image.fromarray(image)
 
         return rgb_image
 
     def name_generator(self):
+        """
+        Generates a random image name.
+
+        Returns:
+            dict: Returns a dict containing both the full file name and the image name.
+        """        
         # Printing lowercase
         letters = string.ascii_lowercase
         # Generating unique image name for dynamic image display
@@ -41,6 +68,15 @@ class ImageExtract():
         return {'full_name':full_filename, 'name':name}
 
     def img_text_extract(self, image):
+        """
+        Extract text from image.
+
+        Args:
+            image (numpy.ndarray): Numpy array containing image.
+
+        Returns:
+            string: Text in image.
+        """        
         # Extracting text from image
         custom_config = r'-l eng --oem 3 --psm 6'
         text = pytesseract.image_to_string(image,config=custom_config)
@@ -48,7 +84,15 @@ class ImageExtract():
         return text
 
     def text_symbol_remotion(self, image_text):
-        # Remove symbol if any
+        """
+        Removes symbols from extracted text in image.
+
+        Args:
+            image_text (string): Text written in image.
+
+        Returns:
+            string: Clean string without symbols.
+        """        # Remove symbol if any
         characters_to_remove = "!()@—*“>+-/,'|£#%$&^_~"
         new_string = image_text
         for character in characters_to_remove:
@@ -60,6 +104,13 @@ class ImageExtract():
         return new_string
 
     def image_save(self, image_arr, name):
+        """
+        Save image based on app configured path.
+
+        Args:
+            img_array (numpy.ndarray): image array that's going to be saved.
+            name (string): string containing image name.
+        """        
         # Saving image to display in html
         
         img = Image.fromarray(image_arr, 'RGB')
@@ -67,6 +118,12 @@ class ImageExtract():
 
 
     def fit_transform(self):
+        """
+        Fit transform all the necessary functions in the transformation and counting steps for image.
+
+        Returns:
+            dictionary: Returns a dictionary containing the amount cars and busses and the full file name.
+        """        
         
         # instance name
         generated_name = self.name_generator()
